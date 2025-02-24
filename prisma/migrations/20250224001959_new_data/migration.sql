6f0de1e7c370fd3fd136eb7fec41ed5798d68d1a
marawansalaheldeen <marawansalahuldeen@gmail.com>
2025-02-24 14:04:09 +0100

    wizard page

---
+++ prisma/migrations/20250224001959_new_data/migration.sql
@@ -0,0 +1,65 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Post";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "User";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income'
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income',
+    "category" TEXT NOT NULL,
+    "categoryIcon" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "MonthHistory" (
+    "userId" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("day", "month", "year", "userId")
+);
+
+-- CreateTable
+CREATE TABLE "YearHistory" (
+    "userId" TEXT NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("month", "year", "userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_userId_type_key" ON "Category"("name", "userId", "type");
